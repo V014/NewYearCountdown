7a1fb764e77198f1df61aa1d9e1ba70ace2c0d30
V014 <wangakanjala@gmail.com>
2024-01-22 22:41:00 +0200

    adding new tasks window

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+from calendar import calendar
 from cgitb import text
 from datetime import datetime, timedelta
-from turtle import color, position
-import customtkinter
+import customtkinter as ctk
+import tkinter as tk
 
 def new_year_countdown():
     current_time = datetime.now()
@@ -19,24 +20,43 @@ def new_year_countdown():
 
     app.after(1000, new_year_countdown) # refresh the process every second
 
-app = customtkinter.CTk() # declare the window, call it what you like, I went with app
+def resize_font(app, event):
+    new_font_size = max(int(event.width / 30), 10)
+    app.days['font'] = ("Arial", new_font_size)
+
+def create_window():
+    new_window = ctk.CTkToplevel()
+    new_window.title('Tasks')
+    new_window.geometry('300x300')
+    ctk.CTkLabel(new_window, text='Set deadlines').pack()
+    # calendar(new_window, selectmode='day',
+    #                year = 2024,
+    #                month=1,
+    #                day=22).pack()
+    new_window.pack()
+
+app = ctk.CTk() # declare the window, call it what you like, I went with app
 app.geometry("350x350") # declare the size
 app.title("Year Counter") # declare the title
 
-frame = customtkinter.CTkFrame(master=app)
+frame = ctk.CTkFrame(master=app)
 frame.pack(pady=20, padx=60, fill="both", expand=True)
 
-label = customtkinter.CTkLabel(frame, text="", font=("Arial",16)) # declare a label
-label.pack(padx=5, pady=5) # apply it to the window
+label = ctk.CTkLabel(frame, text="", font=("Arial",16)) # declare a label
+label.pack() # apply it to the window
 
-progress = customtkinter.CTkProgressBar(frame, orientation="horizontal", mode="determinate")
+days = ctk.CTkLabel(frame, text="", font=("Arial",154), text_color=("light blue")) # declare a label
+days.pack()
+# days.pack(expand=True, fill=customtkinter.BOTH)
+
+days_used = ctk.CTkLabel(frame, text="Days Used", font=("Arial",14), text_color=("light blue")) # declare a label
+days_used.pack() # apply it to the window
+
+progress = ctk.CTkProgressBar(frame, orientation="horizontal", mode="determinate")
 progress.pack()
 
-days = customtkinter.CTkLabel(frame, text="", font=("Arial",154), text_color=("light blue")) # declare a label
-days.pack(padx=5, pady=0) # apply it to the window
-
-days_used = customtkinter.CTkLabel(frame, text="Days Used", font=("Arial",14), text_color=("light blue")) # declare a label
-days_used.pack(padx=5, pady=5) # apply it to the window
+add_task = ctk.CTkButton(frame, text="Add Task", command=create_window)
+add_task.pack(expand=True)
 
 new_year_countdown() # call the function to see the countdown
 
